916d4998ad3ffbf54488cfab9b80a9ac77c9464e
cho1201 <andy1277@naver.com>
2025-06-05 10:56:31 +0900

    Update header.html

--- header.php
+++ header.php
@@ -10,38 +10,38 @@ $userlevel = $_SESSION['userlevel'] ?? "";
 
 <div id="top">
     <h3>
-        <a href="index.php" style="font-size: 32px; font-family: sans-serif;">🎮 게임랜드</a>
+        <a href="index.html" style="font-size: 32px; font-family: sans-serif;">🎮 게임랜드</a>
     </h3>
 
     <ul id="top_menu">
         <?php if (!$userid) { ?>
-            <li><a href="member_form.php">회원가입</a></li>
+            <li><a href="member_form.html">회원가입</a></li>
             <li> | </li>
-            <li><a href="login_form.php">로그인</a></li>
+            <li><a href="login_form.html">로그인</a></li>
         <?php } else { 
             // 로그인된 상태
             if ($userlevel == 1) { ?>
-                <li><a href="admin.php">관리자 모드</a></li>
+                <li><a href="admin.html">관리자 모드</a></li>
                 <li> | </li>
             <?php } 
             $logged = htmlspecialchars($username) . "(" . htmlspecialchars($userid) . ")님 환영합니다.";
             ?>
             <li><?= $logged ?></li>
             <li> | </li>
-            <li><a href="logout.php">로그아웃</a></li>
+            <li><a href="logout.html">로그아웃</a></li>
             <li> | </li>
-            <li><a href="member_modify_form.php">정보수정</a></li>
+            <li><a href="member_modify_form.html">정보수정</a></li>
         <?php } ?>
     </ul>
 </div>
 
 <nav id="menu_bar">
     <ul>
-        <li><a href="index.php">홈</a></li>
-        <li><a href="board_list.php">게시판</a></li>
-        <li><a href="tetris.php">테트리스</a></li>
-        <li><a href="apple_game.php">사과 게임</a></li>
-        <li><a href="2048.php">2048 게임</a></li>
-        <li><a href="message_form.php">고객센터</a></li>
+        <li><a href="index.html">홈</a></li>
+        <li><a href="board_list.html">게시판</a></li>
+        <li><a href="tetris.html">테트리스</a></li>
+        <li><a href="apple_game.html">사과 게임</a></li>
+        <li><a href="2048.html">2048 게임</a></li>
+        <li><a href="message_form.html">고객센터</a></li>
     </ul>
 </nav>
