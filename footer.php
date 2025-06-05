52233f8333d33ec8a6af85cfb898d71faf61aa4b
cho1201 <andy1277@naver.com>
2025-06-05 11:17:13 +0900

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer_content">
-    <p id="footer_logo">게임랜드 | <span>웹프로그래밍 텀프로젝트</span></p>
-    <ul id="author">
-        <li>개발자 문의 메일</li>
-        <li>- 메일 주소 : jaeyun1784@koreatech.ac.kr</li>
-        <li>- 학생 번호 : 2021136129</li>
-    </ul>
-</div>
